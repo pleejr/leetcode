78da895c3e000055d227e1ec3a9ba58e405e0849
pleejr <preston@pleejr.com>
2025-04-11 09:43:00 -0500

    8 string to integer

---
+++ 8_string_to_integer.py
@@ -0,0 +1,104 @@
+# 8. String to Integer (atoi)
+# MEDIUM
+# https://leetcode.com/problems/string-to-integer-atoi/description/
+
+# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+# The algorithm for myAtoi(string s) is as follows:
+# Whitespace: Ignore any leading whitespace (" ").
+# Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+# Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+# Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+# Return the integer as the final result. 
+
+# Example 1:
+# Input: s = "42"
+# Output: 42
+# Explanation:
+
+# The underlined characters are what is read in and the caret is the current reader position.
+# Step 1: "42" (no characters read because there is no leading whitespace)
+#          ^
+# Step 2: "42" (no characters read because there is neither a '-' nor '+')
+#          ^
+# Step 3: "42" ("42" is read in)
+#            ^
+
+# Example 2:
+# Input: s = " -042"
+# Output: -42
+# Explanation:
+# Step 1: "   -042" (leading whitespace is read and ignored)
+#             ^
+# Step 2: "   -042" ('-' is read, so the result should be negative)
+#              ^
+# Step 3: "   -042" ("042" is read in, leading zeros ignored in the result)
+#                ^
+
+# Example 3:
+# Input: s = "1337c0d3"
+# Output: 1337
+# Explanation:
+# Step 1: "1337c0d3" (no characters read because there is no leading whitespace)
+#          ^
+# Step 2: "1337c0d3" (no characters read because there is neither a '-' nor '+')
+#          ^
+# Step 3: "1337c0d3" ("1337" is read in; reading stops because the next character is a non-digit)
+#              ^
+
+# Example 4:
+# Input: s = "0-1"
+# Output: 0
+# Explanation:
+# Step 1: "0-1" (no characters read because there is no leading whitespace)
+#          ^
+# Step 2: "0-1" (no characters read because there is neither a '-' nor '+')
+#          ^
+# Step 3: "0-1" ("0" is read in; reading stops because the next character is a non-digit)
+#           ^
+
+# Example 5:
+# Input: s = "words and 987"
+# Output: 0
+# Explanation:
+# Reading stops at the first non-digit character 'w'.
+
+# Constraints:
+# 0 <= s.length <= 200
+# s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+
+tests = [
+    ("42", 42),
+    (" -042", -42),
+    ("1337c0d3", 1337),
+    ("0-1", 0),
+    ("words and 987", 0),
+]
+
+def myAtoi(s: str) -> int:
+    # print(s)
+    signs = ["-", "+"]
+
+    result = 0
+    s_stripped = s.strip()
+    signed = 1
+    digits = []
+
+    if s_stripped[0] == "-":
+        signed = -1
+        s_stripped = s_stripped[1:]
+
+    for char in s_stripped:
+        if char.isdigit():
+            digits.append(int(char))
+        else:
+            break
+
+    for digit in digits:
+        result *= 10
+        result += digit
+
+    return result * signed
+
+for test in tests:
+    print(f"Result: {myAtoi(test[0])}, Answer: {test[1]}")
