b4bcdfe37e40c307857985ab8d5cd6681159686b
pleejr <preston@pleejr.com>
2025-04-11 09:53:33 -0500

    updates to 8

--- 8_string_to_integer.py
+++ 8_string_to_integer.py
@@ -8,7 +8,7 @@
 # Whitespace: Ignore any leading whitespace (" ").
 # Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
 # Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
-# Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+# Rounding: If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
 # Return the integer as the final result. 
 
 # Example 1:
@@ -73,19 +73,25 @@ tests = [
     ("1337c0d3", 1337),
     ("0-1", 0),
     ("words and 987", 0),
+    ("-91283472332", -2147483648),
+    ("+1", 1),
+    ("", 0),
+    (" ", 0),
 ]
 
 def myAtoi(s: str) -> int:
-    # print(s)
-    signs = ["-", "+"]
-
     result = 0
     s_stripped = s.strip()
     signed = 1
     digits = []
 
+    if not s_stripped:
+        return 0
+
     if s_stripped[0] == "-":
         signed = -1
+        s_stripped = s_stripped[1:]
+    elif s_stripped[0] == "+":
         s_stripped = s_stripped[1:]
 
     for char in s_stripped:
@@ -98,7 +104,14 @@ def myAtoi(s: str) -> int:
         result *= 10
         result += digit
 
-    return result * signed
+    result *= signed
+    
+    if result > (2**31)-1:
+        return (2**31)-1
+    elif result < -(2**31):
+        return -(2**31)
+    else:
+        return result
 
 for test in tests:
     print(f"Result: {myAtoi(test[0])}, Answer: {test[1]}")
